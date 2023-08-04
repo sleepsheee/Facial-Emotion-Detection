3bfec139197548374f3ac62ca1ce6336d94fb3e8
sleepsheee <sleepshee29@gmail.com>
2023-08-04 11:47:10 -0700

    finish

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Facial-Emotion-Detection
+## Facial-Emotion-Detection
+
+# Facial Emotionn detection using JavaScript.
+
+# Use face-api.js library (a wrapper around tensorflow).
+
+# 1. Minifed version of face-api
+
+https://github.com/justadudewhohacks/face-api.js/blob/master/dist/face-api.min.js
+
+# 2. Model files
+
+https://github.com/justadudewhohacks/face-api.js/tree/master/weights
---
+++ script.js
@@ -0,0 +1,40 @@
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  // recognize different part of face
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  // the box of face
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  // recognize facial emotion
+]).then(startVideo);
+
+//hook up webcam with video
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
+}
+startVideo();
+
+video.addEventListener("play", () => {
+  console.log("test");
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height); //clear before draw
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+});
